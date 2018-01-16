adf0da9589c2c1956e0daa891723417b34cd86fa
Yoshikazu Nagai <ynagai@sl6anl01.colorado.edu>
2018-01-16 14:23:01 -0700

    new file from Andy's code

---
+++ style.c
@@ -0,0 +1,18 @@
+{
+ gStyle->SetHistLineWidth(3);
+ gStyle->SetNumberContours(100);
+ gStyle->SetMarkerStyle(8);
+ gStyle->SetOptStat(0);
+ gStyle->SetOptTitle(0);
+ gStyle->SetTitleSize(0.041);
+ gStyle->SetTitleSize(0.041,"Y");
+ gStyle->SetTitleSize(0.041,"Z");
+ gStyle->SetLabelSize(0.041);
+ gStyle->SetLabelSize(0.041,"Y");
+ gStyle->SetLabelSize(0.041, "Z");
+
+
+
+}
+
+
